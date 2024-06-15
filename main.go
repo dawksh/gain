395c3aad6bb36ae599fe10606fb1e2e35150113a
daksh <akshd@outlook.com>
2024-06-15 16:08:49 +0530

    feat: tie everything up

--- main.go
+++ main.go
@@ -15,12 +15,24 @@ import (
 	"github.com/joho/godotenv"
 )
 
+// Helpers
+func respondWithJSON(w http.ResponseWriter, r *http.Request, code int, payload interface{}) {
+	response, err := json.MarshalIndent(payload, "", "  ")
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("HTTP 500: Internal Server Error"))
+		return
+	}
+	w.WriteHeader(code)
+	w.Write(response)
+}
+
 type Block struct {
-	Index int
+	Index     int
 	Timestamp string
-	BPM int
-	Hash string
-	PrevHash string
+	BPM       int
+	Hash      string
+	PrevHash  string
 }
 
 var Blockchain []Block
@@ -47,7 +59,7 @@ func generateBlock(oldBlock Block, BPM int) (Block, error) {
 }
 
 func isBlockValid(oldBlock, newBlock Block) bool {
-	if oldBlock.Index +1 != newBlock.Index {
+	if oldBlock.Index+1 != newBlock.Index {
 		return false
 	}
 	if oldBlock.Hash != newBlock.PrevHash {
@@ -67,11 +79,11 @@ func replaceChain(newBlocks []Block) {
 }
 
 func run() error {
-	var mux := makeMuxRouter()
-	var httpAddr = os.Getenv("ADDR")
+	mux := makeMuxRouter()
+	httpAddr := os.Getenv("ADDR")
 	log.Println("Listening on PORT: ", os.Getenv("ADDR"))
 	s := &http.Server{
-		Addr: ":" + httpAddr,
+		Addr:           ":" + httpAddr,
 		Handler:        mux,
 		ReadTimeout:    10 * time.Second,
 		WriteTimeout:   10 * time.Second,
@@ -86,8 +98,8 @@ func run() error {
 
 func makeMuxRouter() http.Handler {
 	muxRouter := mux.NewRouter()
-	muxRouter.HandleFunc('/', handleGetBlockchain).Methods("GET")
-	muxRouter.HandleFunc('/', handleWriteBlock).Methods("POST")
+	muxRouter.HandleFunc("/", handleGetBlockchain).Methods("GET")
+	muxRouter.HandleFunc("/", handleWriteBlock).Methods("POST")
 	return muxRouter
 }
 
@@ -105,5 +117,40 @@ type Message struct {
 }
 
 func handleWriteBlock(w http.ResponseWriter, r *http.Request) {
-	
+	var m Message
+
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&m); err != nil {
+		respondWithJSON(w, r, http.StatusBadRequest, r.Body)
+		return
+	}
+	defer r.Body.Close()
+
+	newBlock, err := generateBlock(Blockchain[len(Blockchain)-1], m.BPM)
+	if err != nil {
+		respondWithJSON(w, r, http.StatusInternalServerError, m)
+		return
+	}
+	if isBlockValid(Blockchain[len(Blockchain)-1], newBlock) {
+		newBlockchain := append(Blockchain, newBlock)
+		replaceChain(newBlockchain)
+		spew.Dump(Blockchain)
+	}
+
+	respondWithJSON(w, r, http.StatusCreated, newBlock)
+}
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	go func() {
+		t := time.Now()
+		genesisBlock := Block{0, t.String(), 0, "", ""}
+		spew.Dump(genesisBlock)
+		Blockchain = append(Blockchain, genesisBlock)
+	}()
+	log.Fatal(run())
 }
